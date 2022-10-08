a2d4cde8fdb7cc2249bd192ccb000f05a844ab49
pasha01996 <x4i8i15i16i23i42@gmail.com>
2022-10-09 01:26:45 +0300

    feat: check password

--- index.js
+++ index.js
@@ -28,73 +28,96 @@
 // alert(everySecond);
 // alert(everyFourth);
 
-//Свап авторизации на регистрацию
-const linkSignInEl = document.getElementById('link_sign-in')
-const linkSignUpEl = document.getElementById('link_sign-up')
-const formSignInEl = document.getElementById('form_sign-in')
-const formSignUpEl = document.getElementById('form_sign-up')
 
-linkSignInEl.addEventListener('click', change)
-linkSignUpEl.addEventListener('click', change)
+//Switch singIN/singUp
+const buttonSwitchEl = document.querySelectorAll('.form__button_switch')
+const formSignInEl = document.querySelector('.form__sign_in')
+const formSignUpEl = document.querySelector('.form__sign_up')
 
-function change() {
-    formSignInEl.classList.toggle('not_active')
-    formSignUpEl.classList.toggle('not_active')
+buttonSwitchEl[0].addEventListener('click', () => switchForm(formSignInEl, formSignUpEl))
+buttonSwitchEl[1].addEventListener('click', () => switchForm(formSignInEl, formSignUpEl))
+
+function switchForm(elem1, elem2) {
+    elem1.classList.toggle('not_active')
+    elem2.classList.toggle('not_active')
 }
 
-//Закрытие модалльного окна
-window.onclick = function(event) {
-    let target = event.target
-    if (target == modalEl || target == modalContentEl) {
-      modalEl.style.display = "none";
-    }
-  }
 
-// Регистрация
-document.getElementById('button_sign-up').onclick = checkSignUpData
+// SignUp
+const modalEl = document.querySelector('.modal')
+const modalContentEl = document.querySelector('.modal__content')
+const modalVelueEl = document.querySelector('.modal__text')
+const emailSignUpEl = document.querySelector('.form__input_sign_up_email')
+const passSignUpEl = document.querySelector('.form__input_sign_up_password')
 
-const regexEmail = /^([A-Za-z0-9_\-\.]{3,})+\@([A-Za-z0-9_\-\.]{3,20})+\.([A-Za-z]{2,4})/
-const regexPass = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
-const modalEl = document.getElementById('modal')
-const modalContentEl = document.getElementById('modal-content')
-const modalText = document.getElementById('modal-text')
-const emailSignUpEl = document.getElementById('input_sign-up_email')
-const passSignUpEl = document.getElementById('input_sign-up_pass')
+document.querySelector('.form__button_sign_up').addEventListener('click', () => checkInputData(emailSignUpEl, passSignUpEl, modalEl, modalVelueEl))
 
-//Проверка email и пароль
-function checkSignUpData() {
-    if (regexEmail.test(emailSignUpEl.value) && regexPass.test(passSignUpEl.value) ) {
-        modalEl.style.display = 'block'
+function checkInputData(inputEmail, inputPass, modalBody, modalText) {
+    
+    let regexEmail = /^([A-Za-z0-9_\-\.]{3,})+\@([A-Za-z0-9_\-\.]{3,20})+\.([A-Za-z]{2,4})/
+    let regexPass = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+    
+    if (regexEmail.test(inputEmail.value) && regexPass.test(inputPass.value) ) {
+        modalBody.style.display = 'block'
         modalText.innerText = 'Успешная регистрация!'
-        window.localStorage.setItem('login', JSON.stringify(emailSignUpEl.value))
-        window.localStorage.setItem('password', JSON.stringify(passSignUpEl.value))
+        localStorage.setItem('login', JSON.stringify(inputEmail.value))
+        localStorage.setItem('password', JSON.stringify(inputPass.value))
+
     } else {
-        modalEl.style.display = 'block'
-        modalText.innerText = 'Введите корректный Email или пароль'
+        modalBody.style.display = 'block'
+        modalVelueEl.innerText = 'Введите корректный Email или пароль'
     }
 }
 
 
-// Авторизация 
-document.getElementById('button_sign-in').onclick = checkSignIpData
+// SignIn
+const emailSignInEl = document.querySelector('.form__input_sign_in_email')
+const passSignInEl = document.querySelector('.form__input_sign_in_password')
 
-const emailSignIn = document.getElementById('input_sign-in_email')
-const passSignIn = document.getElementById('input_sign-in_pass')
+document.querySelector('.form__button_sign_in').addEventListener('click', () => checkSignIpData(emailSignInEl, passSignInEl, modalEl, modalVelueEl))
 
-function checkSignIpData() {
-    let emailStorage = window.localStorage.getItem('login')
-    let passwordStorage = window.localStorage.getItem('password')
+function checkSignIpData(inputEmail, inputPassword, modalBody, modalText) {
 
-    if (JSON.parse(emailStorage) === emailSignIn.value && JSON.parse(passwordStorage) === passSignIn.value) {
-        modalEl.style.display = 'block'
+    let emailStorage = localStorage.getItem('login')
+    let passwordStorage = localStorage.getItem('password')
+
+    if (JSON.parse(emailStorage) === inputEmail.value && JSON.parse(passwordStorage) === inputPassword.value) {
+        modalBody.style.display = 'block'
         modalText.innerText = 'Успешная авторизация!'
+        inputEmail.value = ''
+        inputPassword.value = ''
     } else {
-        modalEl.style.display = 'block'
+        modalBody.style.display = 'block'
         modalText.innerText = 'Попробуйте другой Email или пароль'
     }
 }
 
 
+//Password Viewer
+const passwordViewerEl = document.querySelectorAll('.form__button_password_viewer')
+
+passwordViewerEl[0].onmousedown = () => passSignInEl.type = 'text'
+passwordViewerEl[0].onmouseup = () => passSignInEl.type = 'password'
+passwordViewerEl[1].onmousedown = () => passSignUpEl.type = 'text'
+passwordViewerEl[1].onmouseup = () => passSignUpEl.type = 'password'
 
 
+//Close modal window
+window.onclick = function(event) {
+    if (event.target == modalEl || event.target == modalVelueEl || event.target == modalContentEl) {
+        modalEl.style.display = "none";
+    }
+}
 
+
+//Forgot password
+document.querySelector('.form__link_forgot').addEventListener('click', checkPassword)
+
+function checkPassword() {
+    let a = localStorage.getItem('login')
+    let b = localStorage.getItem('password') 
+    
+    modalEl.style.display = 'block'
+    modalVelueEl.innerText = `Your Email: ${a} 
+                            Your password: ${b}`
+}
