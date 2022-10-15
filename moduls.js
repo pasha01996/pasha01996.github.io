31615672265aa796f9ac2449c0ef3163f974a7d1
pasha01996 <x4i8i15i16i23i42@gmail.com>
2022-10-15 16:30:12 +0300

    feat: exercise Reborn and check

---
+++ moduls.js
@@ -0,0 +1,44 @@
+const moduls = {
+
+switchForm: function(form1, form2) {
+    form1.classList.toggle('not_active')
+    form2.classList.toggle('not_active')
+},
+
+
+isValidSignUp: function(regexp, input) {
+    return regexp.test(input.value)
+},
+
+addToStorage: function(emailValue, passwordValue, phoneValue, cityValue) {
+    localStorage.setItem('email', JSON.stringify(emailValue.value))
+    localStorage.setItem('password', JSON.stringify(passwordValue.value))
+    localStorage.setItem('phone', JSON.stringify(phoneValue.value))
+    localStorage.setItem('city', JSON.stringify(cityValue.value))
+},
+
+isValidSignIn: function(inputEmail, inputPass) {
+    let emailStorage = localStorage.getItem('email')
+    let passwordStorage = localStorage.getItem('password')
+    return (JSON.parse(emailStorage) === inputEmail.value && JSON.parse(passwordStorage) === inputPass.value)
+},
+
+createModal: function(modal, modalText, text) {
+    modal.style.display = 'block'
+    modalText.innerText = text
+
+},
+
+checkPassword: function(modal, modalText) {
+    let a = localStorage.getItem('email')
+    let b = localStorage.getItem('password') 
+    
+    this.createModal(modal, modalText,`Your Email: ${a} \n Your password: ${b}`)
+},
+
+closeModal: function(event, modal, modalText) {
+    (event.target == modal || event.target == modalText) ? modal.style.display = "none" : false
+}
+}
+
+export default moduls
